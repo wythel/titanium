9164f25ea7b595d50a68ecceeacba6bcdf9e1c65
Wythe Lin <clin@splunk.com>
2016-12-01 12:06:15 +0800

    replace tab to space

--- test/splunk/test_splunk_config.py
+++ test/splunk/test_splunk_config.py
@@ -7,24 +7,24 @@ splunk_home = '/home/eserv/titanium/splunk'
 
 
 class TestSplunkConfig(object):
-	'''
-	Test configuring splunk configs
-	'''
-	def setup_class(cls):
-		cls.installer = installer.install(pkg_url, splunk_home)
-		cls.splunk = Splunk(splunk_home=splunk_home)
+    '''
+    Test configuring splunk configs
+    '''
+    def setup_class(cls):
+        cls.installer = installer.install(pkg_url, splunk_home)
+        cls.splunk = Splunk(splunk_home=splunk_home)
 
-	def teardown_class(cls):
-		# cls.installer.uninstall()
+    def teardown_class(cls):
+        # cls.installer.uninstall()
 
-	def test_read_write_config(self):
-		'''
-		test read and write config file
-		'''
-		# write to savedsearches.conf
-		name = 'test_titanium'
-		data = {'search': 'search *'}
-		self.splunk.edit_conf_file('savedsearches', name, data)
+    def test_read_write_config(self):
+        '''
+        test read and write config file
+        '''
+        # write to savedsearches.conf
+        name = 'test_titanium'
+        data = {'search': 'search *'}
+        self.splunk.edit_conf_file('savedsearches', name, data)
 
-		content = self.splunk.read_conf_file('savedsearches', name)
-		assert content['search'] == data['search']
+        content = self.splunk.read_conf_file('savedsearches', name)
+        assert content['search'] == data['search']
