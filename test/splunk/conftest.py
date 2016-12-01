76e3e9818dd43e3a5fe00479af31ef572d3d2d57
Wythe Lin <clin@splunk.com>
2016-12-01 16:01:49 +0800

    use fixtures instead of setup/teardown cls

--- test/splunk/conftest.py
+++ test/splunk/conftest.py
@@ -1,18 +1,31 @@
 import pytest
+from titanium import installer
+from titanium.splunk import Splunk
+
 
 def pytest_addoption(parser):
     parser.addoption("--splunk-home", default="/opt/splunk",
-    	dest="splunk_home", help="where to install splunk")
+        dest="splunk_home", help="where to install splunk")
     parser.addoption("--skip-install-splunk", action='store_true',
-    	dest='skip_install_splunk',
-    	help="add this option if splunk is ready to be tested")
+        dest='skip_install_splunk',
+        help="add this option if splunk is ready to be tested")
     parser.addoption("--pkg-url", dest="pkg_url",
-    	help="url to the package for testing")
+        help="url to the package for testing")
 
-def pytest_configure(config):
-	'''
-	config pytest
-	'''
-	config.splunk_home = config.getvalue('splunk_home')
-	config.skip_install_splunk = config.getvalue('skip_install_splunk')
-	config.pkg_url = config.getvalue('pkg_url')
+
+@pytest.fixture(scope='session')
+def install_splunk(request):
+    config = request.config
+
+    if not config.getoption('--skip-install-splunk'):
+        my_installer = installer.install(
+            config.getoption('--pkg-url'), config.getoption('--splunk-home'))
+        yield my_installer
+        my_installer.uninstall()
+    yield None
+
+
+@pytest.fixture(scope='session')
+def splunk(request):
+    config = request.config
+    yield Splunk(config.getoption('--splunk-home'))
--- test/splunk/test_splunk_config.py
+++ test/splunk/test_splunk_config.py
@@ -1,31 +1,17 @@
-import pytest
 from titanium import installer
 from titanium.splunk import Splunk
-
-
-pkg_url = pytest.config.pkg_url
-splunk_home = pytest.config.splunk_home
-skip_installation = pytest.config.skip_install_splunk
 
 
 class TestSplunkConfig(object):
     '''
     Test configuring splunk configs
     '''
-    def setup_class(cls):
-        if not skip_installation:
-            cls.installer = installer.install(pkg_url, splunk_home)
-        cls.splunk = Splunk(splunk_home=splunk_home)
 
-    def teardown_class(cls):
-        if not skip_installation:
-            cls.installer.uninstall()
-
-    def test_read_config(self):
+    def test_read_config(self, install_splunk, splunk):
         '''
         test read conf file
         '''
-        content = self.splunk.read_conf_file(
+        content = splunk.read_conf_file(
             'savedsearches', "Errors in the last hour", owner='admin',
             app='search', sharing='app')
         search = ('error OR failed OR severe OR ( sourcetype=access_* '
@@ -33,14 +19,14 @@ class TestSplunkConfig(object):
 
         assert content['search'] == search
 
-    def test_write_config(self):
+    def test_write_config(self, install_splunk, splunk):
         '''
         test write config file
         '''
         # write to savedsearches.conf
         name = 'test_titanium'
         data = {'search': 'search *'}
-        self.splunk.edit_conf_file('savedsearches', name, data)
+        splunk.edit_conf_file('savedsearches', name, data)
 
-        content = self.splunk.read_conf_file('savedsearches', name)
+        content = splunk.read_conf_file('savedsearches', name)
         assert content['search'] == data['search']
