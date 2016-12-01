b445f1c732832733e7fbe2e7b48b9a97b8b80823
Wythe Lin <clin@splunk.com>
2016-12-01 12:03:58 +0800

    add test for splunk

---
+++ test/splunk/test_splunk_config.py
@@ -0,0 +1,30 @@
+from titanium import installer
+from titanium.splunk import Splunk
+
+
+pkg_url = 'http://releases.splunk.com/released_builds/6.3.0/splunk/linux/splunk-6.3.0-aa7d4b1ccb80-Linux-x86_64.tgz'
+splunk_home = '/home/eserv/titanium/splunk'
+
+
+class TestSplunkConfig(object):
+	'''
+	Test configuring splunk configs
+	'''
+	def setup_class(cls):
+		cls.installer = installer.install(pkg_url, splunk_home)
+		cls.splunk = Splunk(splunk_home=splunk_home)
+
+	def teardown_class(cls):
+		# cls.installer.uninstall()
+
+	def test_read_write_config(self):
+		'''
+		test read and write config file
+		'''
+		# write to savedsearches.conf
+		name = 'test_titanium'
+		data = {'search': 'search *'}
+		self.splunk.edit_conf_file('savedsearches', name, data)
+
+		content = self.splunk.read_conf_file('savedsearches', name)
+		assert content['search'] == data['search']
